6e21f8f78277e5de4279f2639dd0a424822c91e5
sonulesagar <sagar@gmail.com>
2023-09-18 15:42:45 +0530

    4-commit

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,8 +1,7 @@
 pipeline {
     agent any 
      stages {
-         stage('code-pull') 
-         {
+         stage('code-pull') {
                 steps 
         {
                        git 'https://github.com/sonulesagar/student-ui.git'
@@ -21,10 +20,10 @@ pipeline {
                 steps {
                         sh 'sudo apt update -y'
                         sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"'
-                        // sh 'sudo apt install unzip -y'
-                        // sh 'unzip awscliv2.zip'
-                        // sh 'sudo ./aws/install'
-                        // sh 'aws s3 ls'
+                        sh 'sudo apt install unzip -y'
+                        sh 'unzip awscliv2.zip'
+                        sh 'sudo ./aws/install'
+                        sh 'aws s3 ls'
                 }
        }
            }
