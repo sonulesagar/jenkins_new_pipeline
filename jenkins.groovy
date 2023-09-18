076c0b9940dea441444e6166f12011c8180340d6
sonulesagar <sagar@gmail.com>
2023-09-18 15:09:55 +0530

    4-commit

--- jenkins.groovy
+++ jenkins.groovy
@@ -5,15 +5,15 @@ pipeline {
          {
                 steps 
         {
-                       git 'https://github.com/sonulesagar/student-ui.git'
-                       echo 'this program is code-pulling'
+                    //    git 'https://github.com/sonulesagar/student-ui.git'
+                    //    echo 'this program is code-pulling'
         }
   }
    stage('Build') {
                 steps {
-                        sh 'sudo apt-get update -y'
-                        sh 'sudo apt-get install maven -y'
-                        sh 'mvn clean package'
+                        // sh 'sudo apt-get update -y'
+                        // sh 'sudo apt-get install maven -y'
+                        // sh 'mvn clean package'
                         
           }
    }      
@@ -22,9 +22,9 @@ pipeline {
                         sh 'sudo apt-get update -y'
                         sh 'sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"'
                         sh 'sudo apt install unzip -y'
-                        sh 'sudo unzip awscliv2.zip'
+                        sh 'unzip awscliv2.zip'
                         sh 'sudo ./aws/install'
-                        sh 'sudo aws s3 ls'
+                        sh 'aws s3 ls'
                 }
        }
            }
